df4d8ccada32dfcbc9da09b4b8ecb52be769ec53
WarriorFromLongAgo <hubeixuegao@gmail.com>
2021-03-29 00:22:36 +0800

    init

---
+++ rocketmq-producer/src/main/java/com/xuegao/rocketmqproduct/transaction/TransactionProducer.java
@@ -0,0 +1,11 @@
+package com.xuegao.rocketmqproduct.transaction;
+
+/**
+ * <br/> @PackageName：com.xuegao.rocketmqproduct.transaction
+ * <br/> @ClassName：TransactionTest
+ * <br/> @Description：
+ * <br/> @author：xuegao
+ * <br/> @date：2021/3/28 20:18
+ */
+public class TransactionProducer {
+}
